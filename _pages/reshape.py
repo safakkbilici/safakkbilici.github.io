17349804bc321441d4942d2327d67e1f977621b7
safak <safakk.bilici@gmail.com>
2022-01-03 14:08:12 +0300

    aaaa

--- _pages/reshape.py
+++ _pages/reshape.py
@@ -2,7 +2,8 @@ import os, cv2
 jpgs = [i for i in os.listdir(".") if i.split(".")[-1] == "jpg"]
 
 for f in jpgs:
-    img = cv2.cvtColor(cv2.imread(f), cv2.COLOR_BGR2RGB)	
+    img = cv2.imread(f)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)	
     img = cv2.resize(img, (370, 499), interpolation = cv2.INTER_CUBIC)
     cv2.imwrite(f, img)
 
